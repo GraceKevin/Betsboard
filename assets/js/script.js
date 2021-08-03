44dac0aa575cd853f2d000498289f4c59a6c72ce
gabriel-perez1 <g.perez.carbo@gmail.com>
2021-08-02 20:25:06 -0400

    stock list js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-fetch(
-	'https://thawing-anchorage-52506.herokuapp.com/https://dashboard.nbshare.io/api/v1/apps/reddit'
-)
-	.then(function(response){
-		return response.json();
-	})
-	.then(function(data){
-		console.log(data);
-	});
-
+function getTicker (){
+	fetch(
+		'https://thawing-anchorage-52506.herokuapp.com/https://dashboard.nbshare.io/api/v1/apps/reddit'
+	)
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(data){
+			var i = 0;
+			while(i < 10){
+				console.log(data[i].ticker)
+				i++
+			}
+		});
+}

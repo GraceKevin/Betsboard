4280c5e4347a184e47d60afae9d2446ee07f0fdd
Kevin Grace <Kevin@kevins-air.home>
2021-08-03 17:39:11 -0400

    quandl search function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,30 @@
 
 //var redditFormElement = document.querySelector("#user-form");
 //var redditContainerElement = document.querySelectorI("#tested");
-//var tickerInputElement = document.querySelector("#user-input")
 
 var apiKEY = "CUsBpcdJn44R3yo3aahR";
 
+document.getElementById("input")
+    .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if ( event.keyCode === 13) {
+            document.getElementById("submitBtn").click();
+        }
+        //removeForecast();
+});
+
 var getQuandl = function() {
-    var quandlAPI = "https://www.quandl.com/api/v3/datasets/WIKI/AAPL/data.json?api_key=" + apiKEY;
-    fetch(quandlAPI)
+    var tickerInputElement = document.querySelector("#input").value;
+
+    //var quandlAPI =;
+    fetch( "https://www.quandl.com/api/v3/datasets/WIKI/" + tickerInputElement + "/data.json?api_key=" + apiKEY)
     .then(function(response) {
         if (response.ok) {
             console.log(response)
             response.json().then(function(data) {
                 console.log(data)
                 //displayTickers();
-        });
+        })
     } else {
         alert("Error, please submit a valid ticker");
     }
